fc60737e2bc52d80f5ce5a87e21f884e7d675d8b
managementbacklog-sys <managementbacklog@gmail.com>
2025-12-28 11:23:31 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB6PUH2DVgwiZrPLWC4yNNjlqqDhdSeM5I",
+    authDomain: "volvopartbacklog.firebaseapp.com",
+    projectId: "volvopartbacklog",
+    storageBucket: "volvopartbacklog.firebasestorage.app",
+    messagingSenderId: "840351131124",
+    appId: "1:840351131124:web:374722674d214c588305a1"
+};
+window.firebaseConfig = firebaseConfig;
